b69c8047d539b6f45cd7633ca35f78e278efe9fe
Kornkanok <thecameanra@gmail.com>
2016-02-23 01:42:46 +0700

    as

--- themes/adminlte/layouts/left.php
+++ themes/adminlte/layouts/left.php
@@ -30,12 +30,12 @@
             [
                 'options' => ['class' => 'sidebar-menu'],
                 'items' => [
-                    ['label' => 'จัดการฐานข้อมูล', 'options' => ['class' => 'header']],
+                    ['label' => 'จัดการฐานข้อมูลข้น', 'options' => ['class' => 'header']],
                     //ตรางจาก db
                     ['label' => 'Bus', 'url' => ['/bus'],],
                     ['label' => 'Descriptions', 'url' => ['/descriptions'],],
                     ['label' => 'Description Van', 'url' => ['/descriptionvan'],],
-                    ['label' => 'ProvinceTH', 'url' => ['/province'],],
+                    // ['label' => 'ProvinceTH', 'url' => ['/province'],],
                     ['label' => 'Station', 'url' => ['/station'],],
                     ['label' => 'Vans', 'url' => ['/van'],],
 
